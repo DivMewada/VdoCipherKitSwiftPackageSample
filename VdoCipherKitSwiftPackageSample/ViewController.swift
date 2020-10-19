87835d53dbaa651f60952e4b1a992be0586163d2
Divya Suthar <divyasuthar1605@gmail.com>
2020-10-19 15:28:07 +0530

    Initial Commit

---
+++ VdoCipherKitSwiftPackageSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VdoCipherKitSwiftPackageSample
+//
+//  Created by iOS Developer on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
